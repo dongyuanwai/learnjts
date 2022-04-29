31bb990bf1c66d9207cd942d5b6cc53d91b8ffdd
dongyuanwai <yuanwaidong@gmail.com>
2022-04-29 22:17:22 +0800

    配置eslint语法

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node":true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+         /* 
+            "off" -> 0 关闭规则
+            "warn" -> 1 开启警告规则
+            "error" -> 2 开启错误规则
+        */
+        "space-before-function-paren": 0,  // 函数定义时括号前面要不要有空格
+        "semi": 0,  // 语句可以不需要分号结尾
+        "eqeqeq": 2, // 必须使用全等
+    }
+}
