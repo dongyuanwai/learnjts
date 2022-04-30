5ad7d2cdb5ab989cee5a4fb327cac5bd1cf1cb4f
dongyuanwai <yuanwaidong@gmail.com>
2022-04-30 22:00:19 +0800

    添加注释规范

--- src/getDataType.js
+++ src/getDataType.js
@@ -1,6 +1,7 @@
 /**
- * params target 目标元素
- * return 字符串
+ * @description: 获取传入变量的数据类型
+ * @param {*} target 传入的目标参数
+ * @return {*} 返回数据类型的小写字符串
  */
 function getDataType(target){
     let type = typeof target
