082a9727cb5194615fbbac250210263228cea273
dongyuanwai <yuanwaidong@gmail.com>
2022-05-01 22:28:12 +0800

    添加深度克隆方法

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+/*
+ * @Author: yuanwai
+ * @Date: 2022-04-28 22:35:16
+ * @LastEditors: yuanwai
+ * @LastEditTime: 2022-05-01 20:39:08
+ * @FilePath: \learnjts\index.js
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by , All Rights Reserved. 
+ */
+
 import getDataType from './src/getDataType.js'
+import deepClone from './src/deepClone.js'
+
+let obj = {
+    a:{b:1},
+    d:Symbol(100)
+}
+console.log("====",obj,deepClone(obj))
 
 export {
-    getDataType
+    getDataType,
+    deepClone
 }
- 
+  
---
+++ src/deepClone.js
@@ -0,0 +1,26 @@
+/**
+ * @description: 
+ * @param {*}
+ * @return {*}
+ */
+
+const isComplexDataType = obj => (typeof obj === 'object' || typeof obj === 'function') && (obj !== null)
+const deepClone = function (obj, hash = new WeakMap()) {
+    // 日期对象直接返回一个新的日期对象
+    if (obj.constructor === Date) return new Date(obj)
+    //正则对象直接返回一个新的正则对象   
+    if (obj.constructor === RegExp) return new RegExp(obj)
+    //如果循环引用了就用 weakMap 来解决
+    if (hash.has(obj)) return hash.get(obj)
+    let allDesc = Object.getOwnPropertyDescriptors(obj)
+    //遍历传入参数所有键的特性
+    let cloneObj = Object.create(Object.getPrototypeOf(obj), allDesc)
+    //继承原型链
+    hash.set(obj, cloneObj)
+    for (let key of Reflect.ownKeys(obj)) { 
+        cloneObj[key] = (isComplexDataType(obj[key]) && typeof obj[key] !== 'function') ? deepClone(obj[key], hash) : obj[key]
+    }
+    return cloneObj
+}
+
+export default deepClone
