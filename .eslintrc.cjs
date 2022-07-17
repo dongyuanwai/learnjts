dfdbf835fb8e25922e4a8518c258c709683755c5
dongyuanwai <yuanwaidong@gmail.com>
2022-07-17 22:09:46 +0800

    fix: eslint与jest冲突

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "node":true,
         "es6": true,
-        "jest/globals": true
+        "jest": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
